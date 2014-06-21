fc12c64af30a6476de6b1fc60d0c729b1d26e189
LobaSakaO <b99901099@ntu.edu.tw>
2014-06-21 18:04:59 +0800

    bind OK diffuse生產中

--- polarizeCell/main.m
+++ polarizeCell/main.m
@@ -1,7 +1,7 @@
 clf;
 
-Xlim = 15;
-Ylim = 10;
+Xlim = 50;
+Ylim = 30;
 [Xmap, Ymap] = meshgrid(1:Xlim+1, 1:Ylim+1);
 Map = zeros(Xlim, Ylim);
 h = surf(Xmap, Ymap, [Map Map(:, Ylim);Map(Xlim, :) 0]');
@@ -10,25 +10,34 @@ axis([1 Xlim+1 1 Ylim+1 0 5 0 2]);
 set(gca, 'View', [0 90], 'XGrid','off');
 
 cell = PopZCell();
+cell.handle_graph = h;
 cell.draw(h);
-pause(1);
+% pause(1);
+
 cell.generate();
 cell.draw(h);
-pause(1);
+% pause(1);
+cell.bind();
+cell.draw(h);
+% pause(1);
+
 cell.generate();
 cell.draw(h);
-pause(1);
-cell.generate();
+% pause(1);
+cell.bind();
 cell.draw(h);
+% pause(1);
 
-pause(1);
-cell.degrade();
-cell.draw(h);
+cell.PopZList.length()
 
-pause(1);
-cell.degrade();
-cell.draw(h);
-
-pause(1);
-cell.degrade();
-cell.draw(h);
+% pause(1);
+% cell.degrade();
+% cell.draw(h);
+% 
+% pause(1);
+% cell.degrade();
+% cell.draw(h);
+% 
+% pause(1);
+% cell.degrade();
+% cell.draw(h);
